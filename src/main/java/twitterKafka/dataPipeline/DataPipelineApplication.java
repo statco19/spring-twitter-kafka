7f8b75676a3b31d65046840d3c25a5cb16026412
andrew <statco19@gmail.com>
2022-02-26 19:13:12 +0900

    SOLVED THE ISSUE: give two different ports as "-Dserver:port=8082, -Dserver:port=8083" at "edit configuration" and run the main app on the port 8081

--- src/main/java/twitterKafka/dataPipeline/DataPipelineApplication.java
+++ src/main/java/twitterKafka/dataPipeline/DataPipelineApplication.java
@@ -4,7 +4,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import org.springframework.kafka.core.KafkaTemplate;
 import twitterKafka.twitter.consumer.SpringConsumerApplication;
 import twitterKafka.twitter.producer.SpringProducerApplication;
 
@@ -15,13 +14,5 @@ public class DataPipelineApplication{
 
 	public static void main(String[] args) {
 		SpringApplication.run(DataPipelineApplication.class, args);
-//		SpringApplication application = new SpringApplication(DataPipelineApplication.class);
-//		application.run(args);
-
-//		SpringApplication producerApplication = new SpringApplication(SpringProducerApplication.class);
-//		producerApplication.run(args);
-
-//		SpringApplication consumerApplication = new SpringApplication(SpringConsumerApplication.class);
-//		consumerApplication.run(args);
 	}
 }
