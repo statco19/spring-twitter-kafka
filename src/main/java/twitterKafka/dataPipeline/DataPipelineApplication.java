0f94b0f57f84756b47ff611ab02c3b436861c3e2
andrew <statco19@gmail.com>
2022-02-27 15:45:17 +0900

    removed RequiredArgsConstructor in DataPipelineApplication.java and created application.yml in a test file

--- src/main/java/twitterKafka/dataPipeline/DataPipelineApplication.java
+++ src/main/java/twitterKafka/dataPipeline/DataPipelineApplication.java
@@ -1,11 +1,9 @@
 package twitterKafka.dataPipeline;
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@RequiredArgsConstructor
 public class DataPipelineApplication{
 
 	public static void main(String[] args) {
